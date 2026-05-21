a744645b719f4e4b9da2ab378033fb19979a8bb5
MalickDevWeb <malickteuw.devweb@gmail.com>
2026-05-21 18:53:20 +0000

    apres the firt

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TransactionCard } from './transaction-card/transaction-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TransactionCard],
+  standalone:true,
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('wallet');
---
+++ src/app/transaction-card/transaction-card.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+
+Component({
+  selector: 'app-transaction-card',
+  standalone:true,
+  templateUrl: './transaction-card.html',
+  styleUrl: './transaction-card.scss',
+})
+
+ interface Transaction {
+  id: number;
+  libelle: string;
+  montant: number;
+  motif:string;
+  type:"credit"|"debit";
+  categorie:"salaire"|"nouriture"|"transport";
+
+
+}
+export class TransactionCard {
+  transactions: Transaction[] = [
+    {
+      id: 1,
+      libelle: 'Paiement facture',
+      montant: 15000,
+      motif: 'thiokh',
+      type: 'credit',
+      categorie: 'salaire',
+    },
+    {
+      id: 2,
+      libelle: 'Paiement facture',
+      montant: 15000,
+      motif: 'thiokh',
+      type: 'credit',
+      categorie: 'salaire',
+    },
+    {
+      id: 3,
+      libelle: 'Paiement facture',
+      montant: 15000,
+      motif: 'thiokh',
+      type: 'credit',
+      categorie: 'salaire',
+    },
+    {
+      id: 4,
+      libelle: 'Paiement facture',
+      montant: 15000,
+      motif: 'thiokh',
+      type: 'credit',
+      categorie: 'salaire',
+    },
+    {
+      id: 5,
+      libelle: 'Paiement facture',
+      montant: 15000,
+      motif: 'thiokh',
+      type: 'debit',
+      categorie:"salaire",
+    },
+    {
+      id: 6,
+      libelle: 'Paiement facture',
+      montant: 15000,
+      motif: 'thiokh',
+      type: 'credit',
+      categorie: 'salaire',
+    },
+    {
+      id: 7,
+      libelle: 'Paiement facture',
+      montant: 15000,
+      motif: 'thiokh',
+      type: "debit",
+      categorie: "transport"
+    },
+  ];
+
+  voirDetails(id: number) {
+    const transaction = this.transactions.find((t) => t.id == id);
+    alert(
+      `Transaction:
+      Libelle: ${transaction?.libelle}
+      Montant:${transaction?.montant}
+      `,
+    );
+  }
+}
