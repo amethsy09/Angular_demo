57f9da5327679295dfc9ae7f6038b74cb9fe82f4
sy <mouhamedsy2221@gmail.com>
2026-06-02 22:51:02 +0000

    ajout formulaire  pour creer une Trans  TERMINER

--- src/app/transaction-card/transaction-card.ts
+++ src/app/transaction-card/transaction-card.ts
@@ -19,11 +19,15 @@ export class TransactionCard {
 
   pageCourante = signal(1);
   transactionsParPage = 5;
-
+  isModalOpen = signal(false);
   idrecherche = signal(0);
   filtreActif = signal('tous');
   libelleRecherche = signal('');
-
+  libelleForm = signal('');
+  montantForm = signal(0);
+  motifForm = signal('');
+  typeForm = signal<'credit' | 'debit'>('credit');
+  categorieForm = signal<'salaire' | 'nouriture' | 'transport'>('salaire');
   transactions = signal<Transaction[]>([
     { id: 1, libelle: 'Paiement facture', montant: 15000, motif: 'thiokh', type: 'credit', categorie: 'salaire' },
     { id: 2, libelle: 'Paiement facture', montant: 15000, motif: 'thiokh', type: 'credit', categorie: 'salaire' },
@@ -34,49 +38,49 @@ export class TransactionCard {
     { id: 7, libelle: 'Paiement facture', montant: 15000, motif: 'thiokh', type: 'debit', categorie: 'transport' }
   ]);
 
- 
-transactionsFiltrees = computed(() => {
-  let data = this.transactions();
-  switch (this.filtreActif()) {
-    case 'CREDIT':
-      data = data.filter(t => t.type === 'credit');
-      break;
-    case 'DEBIT':
-      data = data.filter(t => t.type === 'debit');
-      break;
-  }
 
-  // filtre libellé
-  const libelle = this.libelleRecherche().toLowerCase().trim();
+  transactionsFiltrees = computed(() => {
+    let data = this.transactions();
+    switch (this.filtreActif()) {
+      case 'CREDIT':
+        data = data.filter(t => t.type === 'credit');
+        break;
+      case 'DEBIT':
+        data = data.filter(t => t.type === 'debit');
+        break;
+    }
 
-  if (libelle) {
-    data = data.filter(t =>
-      t.libelle.toLowerCase().includes(libelle)
-    );
-  }
+    // filtre libellé
+    const libelle = this.libelleRecherche().toLowerCase().trim();
 
-  return data;
-});
+    if (libelle) {
+      data = data.filter(t =>
+        t.libelle.toLowerCase().includes(libelle)
+      );
+    }
 
- 
- totalPages = computed(() =>
-  Math.ceil(this.transactionsFiltrees().length / this.transactionsParPage)
-);
+    return data;
+  });
 
-transactionsPaginees = computed(() => {
-  const debut = (this.pageCourante() - 1) * this.transactionsParPage;
-  const fin = debut + this.transactionsParPage;
 
-  return this.transactionsFiltrees().slice(debut, fin);
-});
-  
+  totalPages = computed(() =>
+    Math.ceil(this.transactionsFiltrees().length / this.transactionsParPage)
+  );
+
+  transactionsPaginees = computed(() => {
+    const debut = (this.pageCourante() - 1) * this.transactionsParPage;
+    const fin = debut + this.transactionsParPage;
+
+    return this.transactionsFiltrees().slice(debut, fin);
+  });
+
   boutonPrecedentDesactive = computed(() => this.pageCourante() <= 1);
 
   boutonSuivantDesactive = computed(() =>
     this.pageCourante() >= this.totalPages()
   );
 
- 
+
   precedente() {
     if (this.pageCourante() > 1) {
       this.pageCourante.update(p => p - 1);
@@ -90,7 +94,7 @@ transactionsPaginees = computed(() => {
   }
 
 
-  
+
   totalEntrees = computed(() =>
     this.transactions().filter(t => t.type === 'credit')
       .reduce((s, t) => s + t.montant, 0)
@@ -112,21 +116,73 @@ transactionsPaginees = computed(() => {
     return "Non éligible";
   });
 
-  
+
+  // ajouterTransaction() {
+  //   const nouvelleTransaction: Transaction = {
+  //     // id: this.transactions().length + 1
+  //     id: Math.max(...this.transactions().map(t => t.id), 0) + 1,
+  //     libelle: 'Nouvelle transaction',
+  //     montant: 20000,
+  //     motif: 'achat',
+  //     type: 'debit',
+  //     categorie: 'nouriture'
+  //   };
+
+  //   this.transactions.update(t => [...t, nouvelleTransaction]);
+  // }
   ajouterTransaction() {
+     if (
+    !this.libelleForm().trim() ||
+    this.montantForm() <= 0 ||
+    !this.motifForm().trim()
+  ) {
+    alert("Veuillez remplir correctement le formulaire");
+    return;
+  }
     const nouvelleTransaction: Transaction = {
-      // id: this.transactions().length + 1
       id: Math.max(...this.transactions().map(t => t.id), 0) + 1,
-      libelle: 'Nouvelle transaction',
-      montant: 20000,
-      motif: 'achat',
-      type: 'debit',
-      categorie: 'nouriture'
+      libelle: this.libelleForm(),
+      montant: this.montantForm(),
+      motif: this.motifForm(),
+      type: this.typeForm(),
+      categorie: this.categorieForm()
     };
 
-    this.transactions.update(t => [...t, nouvelleTransaction]);
+    this.transactions.update(list => [...list, nouvelleTransaction]);
+    this.libelleForm.set('');
+    this.montantForm.set(0);
+    this.motifForm.set('');
+    this.typeForm.set('credit');
+    this.categorieForm.set('salaire');
   }
- supprimerTransaction(id: number) {
+  onTypeChange(event: Event) {
+  const value = (event.target as HTMLSelectElement)?.value;
+
+  if (value === 'credit' || value === 'debit') {
+    this.typeForm.set(value);
+  }
+}
+onCategorieChange(event: Event) {
+  const value = (event.target as HTMLSelectElement)?.value;
+
+  if (
+    value === 'salaire' ||
+    value === 'nouriture' ||
+    value === 'transport'
+  ) {
+    this.categorieForm.set(value);
+  }
+}
+ouvrirModal() {
+  this.isModalOpen.set(true);
+
+  this.libelleForm.set('');
+  this.montantForm.set(0);
+  this.motifForm.set('');
+  this.typeForm.set('credit');
+  this.categorieForm.set('salaire');
+}
+  supprimerTransaction(id: number) {
     this.transactions.update(t => t.filter(t => t.id !== id));
   }
 
